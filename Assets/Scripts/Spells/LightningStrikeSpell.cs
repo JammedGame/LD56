23b6c083fbdf2b16f50e342e7c578e65eec6371c
Stefan Stegic <stefan.stegic@foxyvoxel.io>
2024-10-06 17:02:35 +0200

    Add lightning strike visual effect

--- Assets/Scripts/Spells/LightningStrikeSpell.cs
+++ Assets/Scripts/Spells/LightningStrikeSpell.cs
@@ -8,8 +8,11 @@ namespace DefaultNamespace.Spells
 {
     public class LightningStrikeSpell : SpellBattleInstance
     {
+        [SerializeField] private GameObject visualEffectPrefab;
+        
         protected override void Init()
         {
+            Instantiate(visualEffectPrefab, CastTarget, Quaternion.identity);
             Unit closestUnit = GetClosestUnitsInCastArea(CastTarget.ToVector2XZ()).FirstOrDefault();
             if (closestUnit != null)
             {
