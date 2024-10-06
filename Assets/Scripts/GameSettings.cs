7d169061f0161cc4d6cfb24bad3e9824e33e76b9
Misa Jovanovic <misaj@nordeus.com>
2024-10-06 13:36:07 +0200

    GameSettings.Instance

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -8,11 +8,27 @@ namespace DefaultNamespace
 	[CreateAssetMenu]
 	public class GameSettings : ScriptableObject
 	{
+		static GameSettings instance;
+		public static GameSettings Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = Resources.Load<GameSettings>("Config/GameSettings");
+				}
+
+				return instance;
+			}
+		}
+		
 		public const float MaxInputRaycastDepth = 300f;
 		public List<NightLevelData> Levels;
 		public List<SpellBookItem> Spells;
 
 		// Prefabs
 		public PlayerBattleInputManager BattleInputManagerPrefab;
+
+		public List<Vector3> GoodUnitSpawnLocations;
 	}
 }
