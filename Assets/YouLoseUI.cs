0dff6231ed7a8854b1e9763d332c1573585ac76f
TeaElf <teodorapekovic@gmail.com>
2024-10-07 23:47:31 +0200

    add game over sound

--- Assets/YouLoseUI.cs
+++ Assets/YouLoseUI.cs
@@ -7,10 +7,20 @@ public class YouLoseUI : MonoBehaviour
 {
 	public float Duration;	
 	public Animator Animator;	
+	private AudioSource audioSource;
 	
+	void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
 	public IEnumerator Animate(NightBattleContext battleContext)
 	{
 		gameObject.SetActive(true);
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
 		Animator.SetTrigger("Start");
 
 		yield return new WaitForSeconds(Duration);
